2a420d5f6b6df2876daf88199fc3e43e63e220c6
rtrobin <robintang.116@gmail.com>
2022-06-07 07:47:29 +0000

    添加 trojan 协议的抓取

--- utils/fetch_vmess.py
+++ utils/fetch_vmess.py
@@ -5,9 +5,11 @@ import asyncio
 async def fetch_info(msg: str) -> str :
     msg = msg.lower()
 
-    def parse_url(url: str, type: str) -> str:
+    def parse_url(url: str, types: list[str]) -> str:
         full_text = requests.get(url).content.decode()
-        links = re.findall(f'{type}://.+(?=</)', full_text)
+        links = []
+        for type in types:
+            links += re.findall(f'{type}://.+(?=</)', full_text)
 
         ret = ''
         for link in links:
@@ -16,9 +18,11 @@ async def fetch_info(msg: str) -> str :
 
     if msg == 'vmess':
         url = 'https://github.com/Alvin9999/new-pac/wiki/v2ray免费账号'
+        types = ['vmess', 'trojan']
     elif msg == 'ss' or msg == 'ssr':
         url = 'https://github.com/Alvin9999/new-pac/wiki/ss免费账号'
+        types = [msg]
     else:
         return '暂不支持(⊙_⊙)?'
 
-    return await asyncio.to_thread(parse_url, url, msg)
+    return await asyncio.to_thread(parse_url, url, types)
