1f9f74ded0c77322ba0d0a643f0f46e5280717fb
rtrobin <robintang.116@gmail.com>
2022-10-07 14:00:46 +0800

    增加 v2rayN 的订阅接口

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 ﻿from fastapi import FastAPI
+from fastapi.responses import PlainTextResponse
+import base64
 
 from wecom import routes as wecom_router
 from wechat import routes as wechat_router
+from utils.fetch_vmess import fetch_info
 
 app = FastAPI()
 app.include_router(wecom_router.router)
 app.include_router(wechat_router.router)
+
+@app.get("/v2ray")
+async def read_root():
+    vmess = await fetch_info('vmess')
+    vmess_byte = vmess.encode()
+    vmess_b64 = base64.b64encode(vmess_byte)
+    message = vmess_b64.decode()
+    return PlainTextResponse(message)
